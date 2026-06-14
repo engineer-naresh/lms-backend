f8fcd7fc26bfce864cd88c96ce79deb91ee5ee85
engineer-naresh <nareshcodes@gmail.com>
2026-06-14 20:11:40 +0545

    changed the class to functional middleware

--- route/institute/institute.route.ts
+++ route/institute/institute.route.ts
@@ -1,7 +1,7 @@
 import express,{Router} from 'express';
 import  createInstitute from '../../controller/institute/institute.controller.ts';
-import middleware from '../../src/middleware/middleware.ts';
+import isLoggedIn from '../../src/middleware/middleware.ts';
 const router:Router = express.Router();
-router.route('/institute').post(middleware.isLoggedIn ,createInstitute);
+router.route('/institute').post(isLoggedIn ,createInstitute);
 // router.route('/teacher').post(createTeacher);
 export default router;
--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -1,29 +1,41 @@
-import type {Request, Response, NextFunction } from "express";
+import type { Request, Response, NextFunction } from "express";
 import jwt from 'jsonwebtoken';
-class Middleware{
-    static isLoggedIn(req:Request, res:Response, next:NextFunction){
-        const token = req.headers.authorization;
-        if(!token){
-            res.status(401).json({
-                message: "Unauthorized"
-            })
-            return
-        }else{
-            jwt.verify(token,'secrettoken',function(error,result){
-                if(error){
-                    res.status(403).json({
-                        message:"Token invalid"
-                    })
-                }else{
-                     console.log(result, "Result obtained");
-                    
-                }
-            })
-            next();
-        }
-    }   
-    static restricTo(req:Request, res:Response){
-
+import User from "../database/models/user.model.ts";
+interface IExtendedRequest extends Request {
+    user?: {
+        email: string,
+        role: string,
+        username: string | null,
     }
 }
-export default Middleware;
+
+const isLoggedIn = async (req: IExtendedRequest, res: Response, next: NextFunction): Promise<void> => {
+    const token = req.headers.authorization;
+    if (!token) {
+        res.status(403).json({
+            message: "Unauthorized"
+        })
+        return
+    }
+    jwt.verify(token, '', async (error, result: any) => {
+        if (error) {
+            res.status(403).json({
+                message: "Token"
+            })
+        } else {
+            const userData = await User.findByPk(result.id);
+            if (!userData) {
+                res.status(403).json({
+                    message: "No user with token , invalid token"
+                })
+            } else {
+                req.user = { email: userData.email, role: userData.role, username: userData.username, };
+                if (!req.user) {
+                }
+                next();
+            }
+
+        }
+    })
+}
+export default isLoggedIn;
